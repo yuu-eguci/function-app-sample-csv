0ce72180730281ab63d5540cca5e3c4d4d690808
yuu-eguci <yuu.eguci@gmail.com>
2023-04-04 10:11:06 +0900

    ⚗️ flake8 の動作確認完了 成功させる

--- sample_csv_function/__init__.py
+++ sample_csv_function/__init__.py
@@ -6,7 +6,7 @@ import azure.functions as func
 
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
-        tzinfo=datetime.timezone.utc).isoformat() 
+        tzinfo=datetime.timezone.utc).isoformat()
 
     if mytimer.past_due:
         logging.info('The timer is past due!')
